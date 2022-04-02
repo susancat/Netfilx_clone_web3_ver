099088cb1a22c35872a14ee08b0654ae300710da
susancat <zhangjiayu71@gmail.com>
2022-04-02 23:23:53 +0800

    first commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import "./Home.css";
-
+import { Logo } from '../images/Netflix';
+// logo is a svg file
+import { ConnectButton } from 'web3uikit';  
 const Home = () => {
 
 return(
   <>
+  <div className='logo'>
+    <Logo />
+  </div>
+  <div className='connect'>
+    <ConnectButton />
+  </div>
   <div className="container">
      <Link to="/player" className="link">Player Page</Link>
   </div>
